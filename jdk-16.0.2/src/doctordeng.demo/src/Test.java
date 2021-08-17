4fb6ea75f2a5539da68dfbf4e5a3011ad0d71dcb
邓华杰 <vip.doctordeng@gmail.com>
2021-08-17 18:00:09 +0800

    feat: add jdk-16.0.2 source code

---
+++ jdk-16.0.2/src/doctordeng.demo/src/Test.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author DoctorDeng
+ * @version 1.0.0
+ * @date 2021/8/17 17:51
+ */
+public class Test {
+    public static void main(String[] args) {
+        Map<String, String> aaa = new HashMap<>();
+        aaa.put("BBB", "BBB");
+    }
+}
