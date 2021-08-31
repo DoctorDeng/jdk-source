da3b69fb779fde27180f41301842ee0def749f11
邓华杰 <vip.doctordeng@gmail.com>
2021-08-31 16:25:47 +0800

    fix: 解决 jdk 16.0.2 源码部分未提交的问题，现重新提交.

---
+++ jdk-16.0.2/src/jdk.compiler/com/sun/tools/javac/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javac.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "16.0.2+7-67" },
+            { "jdk", "16.0.2" },
+            { "release", "16.0.2" },
+        };
+    }
+}
---
+++ jdk-16.0.2/src/jdk.javadoc/jdk/javadoc/internal/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.javadoc.internal.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "16.0.2+7-67" },
+            { "jdk", "16.0.2" },
+            { "release", "16.0.2" },
+        };
+    }
+}
